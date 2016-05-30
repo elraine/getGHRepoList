7af10e8af5b1825b8beeb833f964ea8a6ed13cf4
elraine <laevatinn@gmail.com>
2016-05-30 15:18:20 +0200

    Can access GitHub API and list of repo from an user. Can handle empty String

---
+++ GetGHRepo/TableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TableViewController.swift
+//  GetGHRepo
+//
+//  Created by Alexis Chan on 30/05/2016.
+//  Copyright © 2016 achan. All rights reserved.
+//
+
+import Foundation
+//
+//class TableViewController: UIViewController,UITableViewDelegate, UITableViewDataSource{
+//    
+//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 0
+//    }
+//    
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        return UITableViewCell()
+//    }
+//    
+//    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+//        
+//    }
+//
+//}
--- GetGHRepo/ViewController.swift
+++ GetGHRepo/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var searchField: UITextField!
+    @IBOutlet weak var lookButton: UIButton!
+    
+    @IBAction func justDoIt(sender: UIButton) {
+        
+        if ((searchField.text) != nil) {
+            if((searchField.text!.isEmpty)){
+                print("lolno")
+                return
+            }
+            
+            let composeReq = "https://api.github.com/search/repositories?q=user:"+searchField.text!+"&sort=updated&order=desc"
+            
+            Alamofire.request(.GET, composeReq , parameters: ["foo": "bar"])
+                .responseJSON { response in
+                    print("URL req")
+                    print(response.request)  // original URL request
+                    print("URL resp")
+                    print(response.response) // URL response
+                    print("URL data")
+                    print(response.data)     // server data
+                    print("results")
+                    print(response.result)   // result of response serialization
+                    
+                    switch response.result {
+                    case .Success:
+                        print("Validation Successful")
+                    case .Failure(let error):
+                        print(error)
+                    }
+                    
+                    if let JSON = response.result.value {
+                        print("JSON: \(JSON)")
+                    }
+            }
+            
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
